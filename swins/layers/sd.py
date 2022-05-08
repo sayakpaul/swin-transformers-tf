c3b48e955c7ca7df6d977b74cce825e1c89b3962
Sayak Paul <spsayakpaul@gmail.com>
2022-05-08 18:25:44 +0530

    feat: enabled model serialization.

--- swins/layers/sd.py
+++ swins/layers/sd.py
@@ -6,12 +6,12 @@ from tensorflow.keras import layers
 class StochasticDepth(layers.Layer):
     def __init__(self, drop_prop, **kwargs):
         super().__init__(**kwargs)
-        self.drop_prob = drop_prop
+        self.drop_prob = float(drop_prop)
 
     def call(self, x, training=False):
         if training:
             keep_prob = 1 - self.drop_prob
-            shape = (tf.shape(x)[0],) + (1,) * (len(tf.shape(x)) - 1)
+            shape = (tf.shape(x)[0],) + (1,) * (tf.shape(tf.shape(x)) - 1)
             random_tensor = keep_prob + tf.random.uniform(shape, 0, 1)
             random_tensor = tf.floor(random_tensor)
             return (x / keep_prob) * random_tensor
