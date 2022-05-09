af99106ea0017fc3ab0f04a8f065feff1773b0fc
Sayak Paul <spsayakpaul@gmail.com>
2022-05-09 06:42:55 +0530

    fix: type annotations.

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,9 +1,7 @@
-from copy import deepcopy
-from typing import Dict, Tuple
+from typing import Union
 
 import numpy as np
 import tensorflow as tf
-from ml_collections import ConfigDict
 
 
 def conv_transpose(w: np.ndarray) -> np.ndarray:
@@ -11,48 +9,12 @@ def conv_transpose(w: np.ndarray) -> np.ndarray:
     return w.transpose(2, 3, 1, 0)
 
 
-def modify_attention_block(
-    qkv: np.ndarray, config: ConfigDict
-) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-    """Modifies the parameters of an attention block so that it's compatible with
-    `layers.mha.TFViTSelfAttention`."""
-    if qkv.ndim == 2:
-        qkv_tf = qkv.T
-        q = qkv_tf[:, : config.projection_dim]
-        k = qkv_tf[:, config.projection_dim : 2 * config.projection_dim]
-        v = qkv_tf[:, -config.projection_dim :]
-    elif qkv.ndim == 1:
-        qkv_tf = deepcopy(qkv)
-        q = qkv_tf[: config.projection_dim]
-        k = qkv_tf[config.projection_dim : 2 * config.projection_dim]
-        v = qkv_tf[-config.projection_dim :]
-    else:
-        raise ValueError(
-            "NumPy arrays with either two or one dimension are allowed."
-        )
-    return q, k, v
-
-
-def get_tf_qkv(
-    pt_component: str, pt_params: Dict[str, np.ndarray], config: ConfigDict
-):
-    """Segregates the query, key, and value subspaces from timm model and makes it
-    compatible to be loaded into `layers.mha.TFViTSelfAttention`."""
-    qkv_weight = pt_params[f"{pt_component}.qkv.weight"]
-    qkv_bias = pt_params[f"{pt_component}.qkv.bias"]
-
-    q_w, k_w, v_w = modify_attention_block(qkv_weight, config)
-    q_b, k_b, v_b = modify_attention_block(qkv_bias, config)
-
-    return (q_w, k_w, v_w), (q_b, k_b, v_b)
-
-
 def modify_tf_block(
-    tf_component: tf.keras.layers.Layer,
+    tf_component: Union[tf.keras.layers.Layer, tf.Variable, tf.Tensor],
     pt_weight: np.ndarray,
     pt_bias: np.ndarray = None,
     is_attn: bool = False,
-) -> tf.keras.layers.Layer:
+) -> Union[tf.keras.layers.Layer, tf.Variable, tf.Tensor]:
     """General utility for modifying PT parameters for TF compatibility.
     Applicable for Conv2D, Dense, tf.Variable, and LayerNormalization.
     """
