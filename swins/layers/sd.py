7a9be0c788e9ed0f2115d4744b302c2a1c8aac90
dc250601 <92249586+dc250601@users.noreply.github.com>
2022-08-01 00:46:55 +0530

    Fixed stochastic depth (#11)

--- swins/layers/sd.py
+++ swins/layers/sd.py
@@ -11,7 +11,7 @@ class StochasticDepth(layers.Layer):
     def call(self, x, training=False):
         if training:
             keep_prob = 1 - self.drop_prob
-            shape = (tf.shape(x)[0],) + (1,) * (tf.shape(tf.shape(x)) - 1)
+            shape = (x.shape[0],)+(1,)*(tf.shape(x).shape[0]-1)
             random_tensor = keep_prob + tf.random.uniform(shape, 0, 1)
             random_tensor = tf.floor(random_tensor)
             return (x / keep_prob) * random_tensor
